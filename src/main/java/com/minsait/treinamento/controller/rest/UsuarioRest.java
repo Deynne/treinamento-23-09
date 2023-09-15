34956e326a04a3f064e5aab60c6d087d81206210
De Andrade Silva, Deynne <dandrades@indracompany.com>
2023-09-15 10:22:11 -0300

    Adicionando Conta e algumas operações de repositório

--- src/main/java/com/minsait/treinamento/controller/rest/UsuarioRest.java
+++ src/main/java/com/minsait/treinamento/controller/rest/UsuarioRest.java
@@ -1,5 +1,8 @@
 package com.minsait.treinamento.controller.rest;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +15,8 @@ import com.minsait.treinamento.model.service.UsuarioService;
 @RequestMapping("usuario")
 public class UsuarioRest extends GenericCrudRestImpl<UsuarioService, Long, UsuarioInsertDTO, UsuarioUpdateDTO, UsuarioDTO> {
 
+    @GetMapping("usuario-{id}")
+    public ResponseEntity<UsuarioDTO> acharPorIdDiferente(@PathVariable Long id) {
+        return ResponseEntity.<UsuarioDTO>ok(this.service.encontrarPorId(id));
+    }
 }
--- src/main/java/com/minsait/treinamento/dtos/usuario/UsuarioInsertDTO.java
+++ src/main/java/com/minsait/treinamento/dtos/usuario/UsuarioInsertDTO.java
@@ -1,6 +1,8 @@
 package com.minsait.treinamento.dtos.usuario;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
@@ -17,4 +19,8 @@ public class UsuarioInsertDTO {
     @NotBlank
     @Size(min = 3, max = 300)
     private String nome;
+    
+    @Positive
+    @NotNull
+    private Double rendaAnual;
 }
